d396de1e870fc7925bc0acef4ed0d61e20710abf
Benjamin Viau <benjamin_viau@yahoo.com>
2019-11-06 22:39:35 +0100

    add_component

--- src/engine.ml
+++ src/engine.ml
@@ -1,51 +1,52 @@
-class ['a] entity  (components : 'a list)= 
+type rectangle = {height : int ; width : int}
+type position = {x : int ; y : int}
+type scale = {factor_height : int ; factor_width : int}
+
+class entity  (components : component list)= 
   object(self)
-    val mutable components : 'a list = components
-    method get_components = components
+    val mutable components : component list = components
+    method add_component c = 
+      components <- c::components
+    method get_components = ref components
   end
 
-class virtual component (entity : component entity ref)= 
+and virtual component (entity :  entity ref)= 
   object(self)
-    val mutable _entity : component entity ref = entity 
-    method virtual update : unit
+    val mutable _entity : entity ref = entity 
+    method get_entity_ref = _entity
+    method update : unit = ()
   end
 
 class virtual action = 
   object(self)
     method virtual perform : unit 
   end
-type rectangle = {height : int ; width : int}
-type position = {x : int ; y : int}
-type scale = {factor_height : int ; factor_width : int}
 
 class virtual actor (components : component list) (cd : int) (acts : action list)= 
   object(self)
-    inherit [component] entity components
+    inherit entity components
     val mutable cooldown = cd
     val mutable actions =acts
     val mutable is_ready  = false
     method virtual take_action : unit  
   end
 
-
-
 class collision_box (box : rectangle)= 
   object(self)
     val mutable box : rectangle = box
   end
 
-class transform (entity :component entity ref)= 
+class transform (entity : entity ref)= 
   object(self)
     inherit component entity
     method update = ()
   end
 
-class scene (entities : component entity list)  = 
+class scene (entities : entity list)  = 
   object(self)
     val mutable entities  = entities
     method gameUpdate = 
     let iter_on_component = 
       List.iter (fun c ->c#update) in
-    List.iter ( fun e -> iter_on_component e#get_components) entities
-    
+    List.iter ( fun e -> iter_on_component !(e#get_components)) entities
   end
--- src/engine.mli
+++ src/engine.mli
@@ -1,47 +1,47 @@
-
-class  type ['component]  entity  = 
-  object
-    val mutable components : 'component list
-    method get_components : 'component list
-  end
-class type virtual component  = 
-  object
-    val mutable _entity : component entity ref
-    method virtual update : unit 
-  end
-
-class type virtual action = 
-  object 
-    method virtual perform : unit
-  end 
-
-
-class type virtual actor = 
-  object
-    inherit [component] entity
-    val mutable cooldown : int
-    val mutable actions : action list
-    val mutable is_ready : bool
-    method virtual take_action : unit
-  end
 type rectangle = {height : int ; width : int}
 type position = {x : int ; y : int}
 type scale = {factor_height : int ; factor_width : int}
 
+class entity  : component list ->
+  object
+    val mutable components : component list
+    method add_component : component -> unit
+    method get_components : (component list) ref
+  end
+and virtual component  : entity ref ->
+  object
+    val mutable _entity : entity ref
+    method get_entity_ref : entity ref
+    method update : unit 
+  end
 
-class type collision_box = 
+class virtual action : 
+  object 
+    method virtual perform : unit
+  end 
+
+class virtual actor : component list -> int -> action list ->
+  object
+    inherit entity
+    val mutable cooldown : int
+    val mutable actions : action list
+    val mutable is_ready : bool
+    method virtual take_action : unit
+  end 
+
+class collision_box :  rectangle ->
   object 
     val box : rectangle
   end
 
-class type transform  = 
+class transform  : entity ref ->
   object 
     inherit component
     method update : unit
   end
 
-class type scene = 
+class scene : entity list ->
   object
-    val mutable entities : (component entity) list
+    val mutable entities : entity list
     method gameUpdate : unit
   end
