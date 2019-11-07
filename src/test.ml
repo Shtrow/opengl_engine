a2f962a03315309f34fc2883872a34bfb2d533d8
Benjamin Viau <benjamin_viau@yahoo.com>
2019-11-07 13:40:28 +0100

    correction boucle

--- src/test.ml
+++ src/test.ml
@@ -5,11 +5,12 @@ let debug_entity =
   object(self)
     inherit entity []
   end
+
 let a_component = 
   object(self)
   inherit component  (ref debug_entity)
   method init = print_string "initialisation"
-  method update  = print_string "I'm alive!"
+  method update  = print_string "I'm alive"; print_newline ();
   end;;
 
 debug_entity#add_component a_component;;
@@ -17,16 +18,16 @@ debug_entity#add_component a_component;;
 
 let scene = new scene [debug_entity];;
 
-let framerate = 1./.60.;;
+(* une image par seconde *)
+let framerate = 1.;; 
 
 let rec gameLoop (scene:scene) (last_time: float) (dt_cumulator:float) : unit= 
   let t = Sys.time() in
   let dt = (t -. last_time) in
 
   (* ici on limite le framerate *)
-  (* if dt_cumulator >= framerate then (scene#sceneUpdate; gameLoop scene t 0.)
-  else *)
-  scene#sceneUpdate;
-  gameLoop scene t (dt_cumulator +. dt)
+  if dt_cumulator >= framerate then (scene#sceneUpdate; gameLoop scene t 0.)
+  else
+  gameLoop scene (t) (dt_cumulator +. dt)
 ;;
 gameLoop scene 0. 0.;;
