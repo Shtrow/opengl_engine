6e03019001b71b1ce1c44bba453b57825bd30f89
Henri Pissa <pissabruney@hotmail.com>
2020-01-30 18:53:29 +0000

    Update Test.ml

--- src/test.ml
+++ src/test.ml
@@ -1,6 +1,64 @@
 open Engine
 
 
+
+type player = {mutable x : int ; mutable y : int };;
+
+
+type field = {map: string array array; a : int ; b : int};;
+
+
+let build x y = {map=Array.make_matrix x y "-"; a=x; b=y};;
+
+
+let g = build 5 7;;
+
+
+let dis() = g.map |> Array.iter (fun xs -> xs|>  Array.iter (fun x -> print_string x); print_newline ());;
+
+
+let set (x,y) z = g.map |> (fun s -> s.(x).(y) <- z);;
+
+set (1,1) "€";;
+
+let move (x, y) p = g.map |> (fun s -> 
+         set (x+p.x, y+p.y) s.(p.x).(p.y); 
+          set (p.x, p.y) "-";
+           p.x <- x+p.x;
+            p.y <- y+p.y);;
+
+
+
+let direction d = match d with
+  | "1"-> (-1,0)
+    | "2"-> (0,1)
+      | "3"-> (0,-1)
+        | "4"-> (1, 0)
+          | _-> (0,0)
+;;
+
+
+let border (x,y) = g |> (fun s -> if x>s.a || x<0 || y>s.b || y <0  then true else false);;
+
+let isEmpty (x,y) = g.map|> (fun s -> if s.(x).(y)= "-" then true else false);;
+
+
+Random.self_init();;
+
+let addObj nObject sym f = 
+         let rec aux obj x y = match obj with
+            | 0 -> ()
+               | _ -> if (isEmpty (x,y) && (border (x,y)<>false)) then 
+                            ((set (x,y) sym); aux (obj-1) (Random.int f.a) (Random.int f.b))
+                                 else (aux obj (Random.int f.a) (Random.int f.b))
+                                   in aux nObject (Random.int f.a) (Random.int f.b);;
+
+
+
+
+
+
+
 let debug_entity = 
   object(self)
     inherit entity []
