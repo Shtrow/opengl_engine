ef93a6ce54143ff39198b39d1833c579495e9819
Benjamin <benjamin.viau@etu-univ-paris-diderot.fr>
2020-01-31 23:30:24 +0100

    Some math basic functions

---
+++ src/Math/vector2.ml
@@ -0,0 +1,29 @@
+type vector = float * float
+
+let rad_to_degree angle = angle *. (180.0 /. Float.pi)
+
+let lenght (v_x,v_y) = 
+  (v_x *.v_x)+.(v_y*.v_y) |> sqrt
+
+let distance (v1_x,v1_y) (v2_x,v2_y) = 
+((v2_x-. v1_x)*.(v2_x-. v1_x)) +.   ((v2_y-. v1_y)*.(v2_y-. v1_y)) |> sqrt
+
+let normalize ((v_x,v_y) as v) = 
+  let l = lenght v in 
+  v_x /. l , v_y /. l
+
+let (-) (u1,u2) (v1,v2) = 
+u1-.v1,u2-.v2
+
+let (+) (u1,u2) (v1,v2) = 
+u1+.v1,u2+.v2
+
+let mul (u1,u2) scalar = 
+u1*.scalar,u2*.scalar
+
+let degree_to_vector angle= 
+let angle = rad_to_degree angle in
+cos angle, sin angle
+
+let vector_to_degree (x,y) = 
+  Float.acos x |> rad_to_degree
